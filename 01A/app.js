e0517a9d4e971bd9a43d9982ffcf536737842aa0
tykoro <37297@ma-web.nl>
2023-11-21 12:04:36 +0100

    les2

--- 01A/app.js
+++ 01A/app.js
@@ -1,8 +1,16 @@
+let appNaam = "Greatest Of App Time"
+let versienummer = 0.1
+let versiedatum = Date(2023, 11, 21)
+let autheur = "Tykoro Mathijssen"
+let copyright = "Tykolo inc.©"
+let distributeur = "Tykolo inc."
+let darkmode = false
+
 class App
 {
     runApplication()
     {
-        console.log("Hello World!");
+        console.log(appNaam,"\nversion:",versienummer,"\n",versiedatum,"\n",autheur,copyright,distributeur,"\n",darkmode);
     }
 }
 
