d4bee339d61f764d06d18a0beb5ac33a0f02c2c1
tykoro <37297@ma-web.nl>
2023-11-23 12:35:48 +0100

    les04canvas

---
+++ 02 huisje/app.js
@@ -0,0 +1,15 @@
+class App
+{
+    runApplication()
+    {
+        let canvas = document.getElementById("canvasId")
+        let g = canvas.getContext("2d");
+        g.fillStyle = "green";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "gold";
+        g.fillRect(0,0,10,10);
+        console.log(canvas)
+    }
+} 
+let app = new App(); 
+app.runApplication(); 
--- 02 lijnen/app.js
+++ 02 lijnen/app.js
@@ -4,15 +4,56 @@ class App
     {
         let canvas = document.getElementById("canvasId")
         let g = canvas.getContext("2d");
+
         g.beginPath();
-        g.fillStyle = "green";
-        g.moveTo(20,20);
-        g.lineTo(300,300)
-        g.lineTo(580,20)
+        g.fillStyle = "gray";
+        g.moveTo(70,20);
+        g.lineTo(80,30)
+        g.lineTo(60,40)
+        g.closePath()
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "red";
+        g.moveTo(30,10);
+        g.lineTo(70,20)
+        g.lineTo(60,40)
+        g.lineTo(20,30)
+        g.closePath()
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "gray";
+        g.moveTo(60,40);
+        g.lineTo(60,60)
+        g.lineTo(20,50)
+        g.lineTo(20,30)
+        g.closePath()
+        g.stroke();
+        g.fill()
+
+        g.beginPath();
+        g.fillStyle = "gray";
+        g.moveTo(60,40);
+        g.lineTo(80,30)
+        g.lineTo(80,50)
+        g.lineTo(60,60)
         g.closePath()
         g.stroke();
         g.fill()
         console.log(canvas)
+
+        g.beginPath();
+        g.fillStyle = "black";
+        g.moveTo(35,35);
+        g.lineTo(45,38)
+        g.lineTo(45,48)
+        g.lineTo(35,45)
+        g.closePath()
+        g.stroke();
+        g.fill()
     }
 } 
 let app = new App(); 
