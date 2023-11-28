6f4983e0fc1df53e1f0bf700f09fb3c77f1636a0
tykoro <37297@ma-web.nl>
2023-11-28 11:57:24 +0100

    les01

---
+++ 03 ifelse/app.js
@@ -0,0 +1,12 @@
+
+class App
+{
+    runApplication()
+    {
+        let GameNews = document.getElementsByClassName('gamenews headline');
+        console.log(GameNews);
+    }
+}
+
+let app = new App();
+app.runApplication();
