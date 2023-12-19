8b4a228075210cab3ef99cb24ddc54482257a614
Elitetykoro <tykoromathijssen@gmail.com>
2023-12-19 12:55:45 +0100

    les06.3

--- 06 events/app.js
+++ 06 events/app.js
@@ -1,15 +1,45 @@
-
 class App
 {
     runApplication()
     {
-        let uiButton = document.getElementById("myButton");
+        let headerButtonvar = document.getElementById("headerButton");
 
-        let localeFunction = function (e)
+        headerButtonvar.addEventListener("click", (e)=>
         {
-            console.log("click");
-        }
-        uiButton.addEventListener("click",localeFunction);
+            const head = document.createElement("h1");
+            const node = document.createTextNode("this is new.");
+            head.appendChild(node);
+            document.body.appendChild(head);
+        });
+
+        let paraButtonvar = document.getElementById("paraButton");
+
+        paraButtonvar.addEventListener("click", (e)=>
+        {
+            const para = document.createElement("p");
+            const node = document.createTextNode("this is new.");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+
+        let imgButtonvar = document.getElementById("imgButton");
+
+        imgButtonvar.addEventListener("click", (e)=>
+        {
+            const img = new Image();
+            img.src = "img/logo-ns.png";
+            document.body.appendChild(img);
+        });
+
+        let divButtonvar = document.getElementById("divId");
+
+        divButtonvar.addEventListener("click", (e)=>
+        {
+            const head = document.createElement("div");
+            const node = document.createTextNode("div Button");
+            head.appendChild(node);
+            document.body.appendChild(head);
+        });
     }
 }
 
